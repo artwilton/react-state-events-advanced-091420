20c928927eb1779612e5c320dc9b985a5b58e754
artwilton <Arthur.M.Wilton@gmail.com>
2020-11-18 14:25:17 -0500

    started populating favorites

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./App.css";
 import BeyContainer from "./Containers/BeyContainer";
 import Favorites from "./Containers/Favorites.js";
 
-
 class App extends React.Component {
 
   state = {
     bey: {},
+    beyArray: []
   }
 
   appClickHandler = (beyObj) => {
-    this.setState({bey: beyObj})
+    this.setState({bey: beyObj}, console.log(this.state.bey.favorite))
     if (this.state.bey.favorite === false) {
       this.state.bey.favorite = true
+      this.state.beyArray.push(this.state.bey)
     }
-    console.log(this.state.bey.favorite)
   }
 
   render() {
@@ -23,7 +23,9 @@ class App extends React.Component {
     return (
       <div className="container" >
         <BeyContainer appClickHandler={this.appClickHandler}/>
-        <Favorites bey={this.state.bey}/>
+        <Favorites beyArray={this.state.beyArray} appClickHandler={this.appClickHandler}/>
+        {/* <Favorites bey={this.state.bey}/> */}
+
       </div>
     
     );
--- src/Containers/Favorites.js
+++ src/Containers/Favorites.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import BeyCard from '../Components/BeyCard'
 
 class Favorites extends Component {
+
+  state = {
+    beyArray: this.props.beyArray
+  }
+
+
+  renderCard = () => {
+    return this.state.beyArray.map(beyObj => <BeyCard key={beyObj.id} bey={beyObj} appClickHandler={this.props.appClickHandler} />)
+   }
+
+
   render() {
     return (
       <div className="favorites">
         <h1>Favorites</h1>
-          <BeyCard key={this.props.bey.id} bey={this.props.bey}/>
+        {this.renderCard()}
+          {/* <BeyCard key={this.props.bey.id} bey={this.props.bey}/> */}
       </div>
     );
   }
 }
+
+// iteratre through cards
+// render to screen which cards have 'favorite: true'
 
 export default Favorites;
